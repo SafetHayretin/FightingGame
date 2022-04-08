f360081b82b5c6a9d070eba5e3d49aa40acca6d5
SafetKyuchyukhalil <82340780+djzombie1@users.noreply.github.com>
2022-04-08 14:53:22 +0300

    Implementation to fighter class

--- src/main/java/Fighter.java
+++ src/main/java/Fighter.java
@@ -1,15 +1,98 @@
-public abstract class Fighter {
+/**
+ * Fighter class with health, armor and attack.
+ * He can attack other fighters.
+ * When fighter attacks he deals damage to another fighter.
+ * If the attacked fighter has armor his armor points went down after the attack.
+ * If fighter is left with no armor points next attack he will take damage to  his health points.
+ * When creating your fighter you can NOT set your health, armor or attack points to negative number.
+ */
+public class Fighter {
     private int health;
-    private int attack;
     private int armor;
+    private final int ATTACK;
 
-    public Fighter(int health, int attack, int armor){
+    /**
+     * Fighter with 3 parameters
+     * @param health can't be negative
+     * @param attack can't be negative
+     * @param armor can't be negative
+     */
+    public Fighter(int health, int attack, int armor) {
+        if(health < 0 || attack < 0 || armor < 0){
+            throw new IllegalArgumentException("Can't assign negative parameters!");
+        }
         this.health = health;
-        this.attack = attack;
+        this.ATTACK = attack;
         this.armor = armor;
     }
 
-    public void attackMove(){
-        
+    /**
+     * A way that your fighter attacks the opponent
+     * @param fighter
+     */
+    public void attackMove(Fighter fighter) {
+        int health = fighter.getHealth();
+        int armor = fighter.getArmor();
+        int damage = getAttack();
+
+        if (isArmorBroken()) {
+            health -= damage;
+            fighter.setHealth(health);
+        } else {
+            armor -= damage;
+            fighter.setArmor(armor);
+
+            if (armor < 0) {
+                health += armor;
+                fighter.setHealth(health);
+            }
+        }
+    }
+
+    /**
+     * Checker if your fighter's armor is broken
+     * If your armor is broken
+     * @return true
+     */
+    public boolean isArmorBroken() {
+        if (armor <= 0) {
+            return true;
+        } else return false;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getAttack() {
+        return ATTACK;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    /**
+     * Updates your health
+     * Can't be negative
+     */
+    public void setHealth(int health) {
+        if (health <= 0) {
+            this.health = 0;
+        } else {
+            this.health = health;
+        }
+    }
+
+    /**
+     * Updates your armor
+     * Can't be negative
+     */
+    public void setArmor(int armor) {
+        if (armor <= 0) {
+            this.armor = 0;
+        } else {
+            this.armor = armor;
+        }
     }
 }
---
+++ src/main/java/Warrior.java
@@ -0,0 +1,8 @@
+
+public class Warrior extends Fighter {
+
+    public Warrior(int health, int attack, int armor) {
+        super(health, attack, armor);
+    }
+
+}
