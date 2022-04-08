dddcd63ac1c2017d9ca24d92e351dda15654b770
Safet <sftaliev@gmail.com>
2022-04-08 10:59:15 +0300

    Update

---
+++ src/main/java/Fighter.java
@@ -0,0 +1,13 @@
+public abstract class Fighter {
+    private int health;
+    private int attack;
+    private int armor;
+
+    public Fighter(int health, int attack, int armor){
+        this.health = health;
+        this.attack = attack;
+        this.armor = armor;
+    }
+
+
+}
